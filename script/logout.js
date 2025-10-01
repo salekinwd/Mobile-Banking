7f2c027b80636e0683df498e86238a39005f1375
salekinwd <salekinshahir789@gmail.com>
2025-10-02 01:16:43 +0600

    2nd time code update

---
+++ script/logout.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-logout").addEventListener("click", function () {
+    // Alert 
+    alert("Are you sure you want to log out?");
+
+    // Login 
+    window.location.href = "index.html";
+});
